4da03077242caf5184ee926056696cb3d964577b
timothyylim <timothylim23@gmail.com>
2018-07-29 14:14:56 +0200

    first commit

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <vue-datepicker-local v-model="time" />
+</template>
+
+<script>
+import VueDatepickerLocal from 'vue-datepicker-local'
+
+export default {
+  components: {
+    VueDatepickerLocal
+  },
+  data () {
+    return {
+      time: new Date()
+    }
+  }
+}
+</script>
